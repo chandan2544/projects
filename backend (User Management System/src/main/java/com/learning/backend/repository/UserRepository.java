30a7447ffb4f1d7e2d01d78d122b89a770d0ee79
chandan kumar <145868696+chandan2544@users.noreply.github.com>
2024-08-11 23:25:49 +0530

    Delete backend (User Management System directory

--- backend (User Management System/src/main/java/com/learning/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learning.backend.repository;
-
-import com.learning.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
