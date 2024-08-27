530296bb57a26551553b2dca75aaff77d8a63655
chandan kumar <145868696+chandan2544@users.noreply.github.com>
2024-08-27 11:45:53 +0530

    Delete Backend-User-Management-System directory

--- Backend-User-Management-System/src/main/java/com/backend/Backend/User/Management/System/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.backend.Backend.User.Management.System.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id){
-        super("Could not found the user with id "+ id);
-    }
-}
